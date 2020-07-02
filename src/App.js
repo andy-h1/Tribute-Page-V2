30df1a34693235f9a86862e68fd3211427fab05f
Andy Huynh <36887000+andy-h1@users.noreply.github.com>
2020-07-02 16:08:19 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,36 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <img src="https://media-exp1.licdn.com/dms/image/C5603AQEPuk_At03p4A/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=_uSedpwD-uOIz1EfWljbIqBCNTel6EC-8Kv8CMsWAMs" alt="Dan Page" />
+      <h4>Dan Page</h4>
+      <ul>
+        <li>Current company: Deliveroo</li>
+        <li>Current position: Front End Engineer</li>  
+      </ul>
+      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFiuliOZ8UHVA/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=Fkbq8EY5mzBs1uRyjSa_JobY8f4ru8cy3cwqaEFRbjc" alt="Joe Hunt" />
+      <h4>Joe Hunt</h4>
+      <ul>
+        <li>Current company: Amazon</li>
+        <li>Current position: Technical Recruiter</li>  
+      </ul>
+      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFW5MIAJM1Ayg/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=o0aujpu11J-MJxSLa_tsrd9mZTH3SuLV0mHVkW2svHA" alt="Nav Garcha" />
+      <h4>Nav Garcha</h4>
+      <ul>
+        <li>Current company: Deliveroo</li>
+        <li>Current position: Staff Engineer</li>  
+      </ul>
+      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFm7kMck_Snmw/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=fCAIyyBt_9AG1ZLVYzAdrehIUy1v3ECL3tq1QR1VLU0" alt="Ollie Page" />
+      <h4>Ollie Page</h4>
+      <ul>
+        <li>Current company: ClearScore</li>
+        <li>Current position: Data Scientist</li>  
+      </ul>
+      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFC-zHCo7vF1Q/profile-displayphoto-shrink_200_200/0?e=1597276800&v=beta&t=dd5hB9T4_7oBflE4eSHjpxR3wcoT5P8hgZ0HuxCKBgU" alt="Pedro Sousa" />
+      <h4>Pedro Sousa</h4>
+      <ul>
+        <li>Current company: ClearScore</li>
+        <li>Current position: Head of Legal & Complicance</li>  
+      </ul>
     </div>
   );
 }
