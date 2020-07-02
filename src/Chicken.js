af594815b791e82d1f75fc39de0a8f0124a52ed5
Andy Huynh <36887000+andy-h1@users.noreply.github.com>
2020-07-02 20:11:33 +0100

    Adding data on the Chicken crew

---
+++ src/Chicken.js
@@ -0,0 +1,51 @@
+//Data of the chicken crew
+
+const chickens = {
+    chick1: {
+        name: "Dan Page",
+        image: "/images/dpage.jpg",
+        company: "Memrise",
+        title: "Front End Engineer",
+        bio: "One of the founders of the Chicken Crew, Dan is often found working on his own side projects or climbing the ranks in Valorant or WoW.",
+        github: "https://github.com/D-Pagey" 
+    },
+
+    chick2: {
+        name: "Joe Hunt",
+        image: "/images/jhunt.jpg",
+        company: "Amazon",
+        title: "Technical Recruiter",
+        bio: "Co-founder of the Chicken Crew, Joe is currently learning how to become a Front End legend like myself. He is the person to turn to when it comes to finding out the latest games or incredible beats/rap music",
+        github: "https://github.com/GlitchHunt" 
+    },
+
+    chick3: {
+        name: "Nav Garcha",
+        image: "/images/ngarcha.jpg",
+        company: "Deliveroo",
+        title: "Staff Engineer",
+        bio: "The Connoisseur of interior design and Front End frameworks, Nav has a fine eye for detail and taste. He is our go to guy when it comes to anything design focused.",
+        github: "https://github.com/navgarcha" 
+    },
+
+    chick4: {
+        name: "Ollie Page",
+        image: "/images/opage.jpg",
+        company: "ClearScore",
+        title: "Data Scientist",
+        bio: "The Brawns and Brains of the group, Ollie is currently working up the ranks to be a Data legend. He is often found training his strategical mind on the chessboard",
+        github: "https://github.com/navgarcha" 
+    },
+
+    chick5: {
+        name: "Pedro Sousa",
+        image: "/images/psousa.jpg",
+        company: "ClearScore",
+        title: "Head of Legal and Compliance",
+        bio: "The Lawyer of the group, Pedro is often found working throughout the night on legal matters in both the real world and in WoW.",
+    },
+
+}
+};
+
+export default chickens;
