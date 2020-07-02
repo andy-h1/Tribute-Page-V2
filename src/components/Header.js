b8f3b9b13120dec3c3bf5e400dbb51971fe7bd45
Andy Huynh <36887000+andy-h1@users.noreply.github.com>
2020-07-02 13:54:44 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Tribute Page</h1>
+                <h3>This is one of the FCC projects which I have built to learn core skills in JS/React</h3>
+        </div>
+    );
+}
+
+export default Header;
